ff4f64217f912233c4b586c1034a357b66872d3a
Santa Cadusch <santacadusch@icloud.com>
2021-04-14 15:48:50 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@ Restaurant.destroy_all
 puts "Creating restaurants..."
 
 10.times do
-  restaurant = Restaurant.create(
+  restaurant = Restaurant.create!(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_name,
     phone_number: Faker::PhoneNumber.cell_phone,
-    category: ['Chinese', 'Italian', 'Japanese', 'French', 'Belgian'].sample
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
     )
   puts "Restaurant #{restaurant.id} is created"
   10.times do
     review = Review.create(
       content: Faker::Address.street_name,
       rating: rand(1..5),
-      restaurant_id: restaurant
+      restaurant_id: restaurant.id
       )
   end
 end
